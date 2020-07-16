c88e7eb5d70b8a0abd42835471f2234df0b1db3c
GitHub-LiuMing <GoogLiuMing@Gmail.com>
2020-07-16 10:52:12 +0800

    Netty demo

---
+++ src/main/java/com/example/demo/netty/NettyClient.java
@@ -0,0 +1,39 @@
+package com.example.demo.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+/**
+ * @Description 
+ * @ClassName NettyClient
+ * @Author 鲸落
+ * @date 2020.07.15 15:46
+ */
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("127.0.0.1", 8888).channel();
+
+        while (true) {
+            channel.writeAndFlush(new Date() + ": hello world!");
+            Thread.sleep(300000);
+        }
+    }
+}
